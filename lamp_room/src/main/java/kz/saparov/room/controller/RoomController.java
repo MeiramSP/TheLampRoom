d20e915ee2e477a2bfe692b53908c0738e0cbe51
MeiramSP <83052178+MeiramSP@users.noreply.github.com>
2021-06-09 19:34:49 +0600

    Update RoomController.java

--- lamp_room/src/main/java/kz/saparov/room/controller/RoomController.java
+++ lamp_room/src/main/java/kz/saparov/room/controller/RoomController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-//import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
